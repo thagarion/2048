87877126c1875e4343c8ec99f889797705dc38f0
thagarion <th.qliphoth@gmail.com>
2024-11-10 22:24:19 +0530

    Update animations

--- scenes/score/score.gd
+++ scenes/score/score.gd
@@ -1,4 +1,12 @@
-extends HBoxContainer
+extends VBoxContainer
+
+var prev_value = 0
 
 func _on_game_score_signal(value):
-	$ScoreNumber.text = str(value)
+	$NumbersContainer/ScoreNumber.text = str(value)
+	var diff = value - prev_value
+	if diff > 0:
+		$NumbersContainer/AddScoreNumber.text = "+" + str(diff)
+		$AnimationPlayer.stop()
+		$AnimationPlayer.play("display")
+	prev_value = value
